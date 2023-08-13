5e6cb6395c8e07ebbb3b7d55604e7f9f3d867863
khalidelwadi <khalid.ellwadi@gmail.com>
2023-08-13 13:40:02 +0100

    first commit

---
+++ First and Last.js
@@ -0,0 +1,26 @@
+function array(string) {
+    // check if string is empty
+    if (string == ""){
+      return null;
+    }
+
+    let result = ""
+    let arr = string.split(',');
+    // remove last and first elements
+    arr.pop();arr.shift()
+
+    // check if array is empty
+    if (arr.length == 0){
+      return null
+    }
+    for (let i = 0; i < arr.length; i++){
+        result += arr[i];
+        // add space if the arr[i] is not equal to the last element in arr
+        if (arr[i] != arr[arr.length - 1]){
+            result += " "
+        }
+    
+    }
+      
+      return result;
+}
